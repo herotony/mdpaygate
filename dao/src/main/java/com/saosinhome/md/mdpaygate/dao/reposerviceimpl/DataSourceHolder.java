c25ee268bbd80c78bb884be56d5936781423569f
herotony <herotony@gmail.com>
2016-07-26 10:37:53 +0800

    继续增加相应数据层配置

---
+++ dao/src/main/java/com/saosinhome/md/mdpaygate/dao/reposerviceimpl/DataSourceHolder.java
@@ -0,0 +1,7 @@
+package com.saosinhome.md.mdpaygate.dao.reposerviceimpl;
+
+/**
+ * Created by wangdalin(闪惠后台研发) on 2016/7/26 9:11.
+ */
+public class DataSourceHolder {
+}
---
+++ dao/src/main/java/com/saosinhome/md/mdpaygate/dao/reposerviceimpl/DynamicDataSource.java
@@ -0,0 +1,7 @@
+package com.saosinhome.md.mdpaygate.dao.reposerviceimpl;
+
+/**
+ * Created by wangdalin(闪惠后台研发) on 2016/7/26 8:59.
+ */
+public class DynamicDataSource {
+}
